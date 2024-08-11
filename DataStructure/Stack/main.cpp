cab22aa53841f2e51d2b015d59f758428952b1a7
mohamedtarikz <mohamedtarik06@gmail.com>
2024-08-12 01:40:29 +0300

    Infix to Postfix DONE

--- DataStructure/Stack/main.cpp
+++ DataStructure/Stack/main.cpp
@@ -4,15 +4,90 @@ using namespace std;
 using namespace stl;
 
 int main() {
-    MyStack<string> st;
-    st.push("Hellooo");
-    st.push("My Name");
-    st.push("Mohamed");
-    cout<<st.top()<<endl<<endl;
-    while(!st.empty()){
-        cout<<st.top()<<endl;
-        st.pop();
+    string in;
+    cout<<"Enter your expression: ";
+    getline(cin,in);
+    MyStack<char> check;
+    for (int i = 0; i < in.size(); ++i) {
+        if(in[i] == '(')
+            check.push(in[i]);
+        else if(in[i] == ')'){
+            if(check.top() == '('){
+                check.pop();
+            }
+            else{
+                check.push(in[i]);
+            }
+        }
     }
-    cout<<st.len()<<endl;
+    if(!check.empty()){
+        cout<<"INVALID EXPRESSION!!"<<endl;
+        return 0;
+    }
+    MyStack<string> operators;
+    vector<string> ans;
+    map<string,int> priority;
+    priority["+"] = 1;
+    priority["-"] = 1;
+    priority["*"] = 2;
+    priority["/"] = 2;
+
+    vector<string> v;
+    string tmp;
+    for (int i = 0; i < in.size(); ++i) {
+        if(in[i] == ' ') {
+            if(tmp.size() > 0)
+                v.emplace_back(tmp);
+            tmp = "";
+        }
+        else if(in[i] == '(' || in[i] == ')' || in[i] == '+' || in[i] == '-' || in[i] == '*' || in[i] == '/'){
+            if(tmp.size() > 0)
+                v.emplace_back(tmp);
+            tmp = "";
+            tmp += in[i];
+            v.emplace_back(tmp);
+            tmp = "";
+        }
+        else{
+            tmp += in[i];
+        }
+    }
+
+    for (auto &itr:v) {
+        if (itr == "+" || itr == "-" || itr == "*" || itr == "/"){
+            if(operators.empty()){
+                operators.push(itr);
+            }
+            else{
+                while(!operators.empty() && priority[operators.top()] >= priority[itr]){
+                    ans.emplace_back(operators.top());
+                    operators.pop();
+                }
+                operators.push(itr);
+            }
+        }
+        else if(itr == "("){
+            operators.push(itr);
+        }
+        else if(itr == ")"){
+            while(operators.top() != "("){
+                ans.emplace_back(operators.top());
+                operators.pop();
+            }
+            operators.pop();
+        }
+        else{
+            ans.emplace_back(itr);
+        }
+    }
+    while(!operators.empty()){
+        ans.emplace_back(operators.top());
+        operators.pop();
+    }
+    for (int i = 0; i < ans.size(); ++i) {
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
