48dfeb886f0dd0d52aa01a5e80e19bafa1eab17c
mohamedtarikz <mohamedtarik06@gmail.com>
2024-09-10 11:44:37 +0300

    ArrayList - DONE

--- DataStructure/ArrayList/MyVector.cpp
+++ DataStructure/ArrayList/MyVector.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
-    MyVector<bool> prime(n+2,true);
-    prime[0] = prime[1] = false;
-    for (int i = 2; i * i <= n; ++i) {
-        if(prime[i]){
-            for (int j = i*i; j <= n; j+=i) {
-                prime[j] = false;
+    while(true) {
+        int n;
+        cin >> n;
+        MyVector<bool> prime(n + 2, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = false;
+                }
             }
         }
-    }
-    MyVector<int> v;
-    for (int i = 2; i <= n; ++i) {
-        if(prime[i]){
-            v.push_back(i);
+        MyVector<int> v;
+        for (int i = 2; i <= n; ++i) {
+            if (prime[i]) {
+                v.push_back(i);
+            }
         }
+//    cout<<"Primes till "<<n<<": \n";
+//    for (auto itr:v) {
+//        cout<<itr<<" ";
+//    }
+//    cout<<endl;
+        cout << v.size() << endl;
+        cout << double(v.size()) * 100 / n << endl;
     }
-    cout<<"Primes till "<<n<<": \n";
-    for (auto itr:v) {
-        cout<<itr<<" ";
-    }
-    cout<<endl;
     return 0;
 }
