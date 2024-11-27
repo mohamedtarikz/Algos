c6820d57e86aaefb2b469f2abe1905ee4224b33d
mohamedtarikz <mohamedtarik06@gmail.com>
2024-11-27 09:43:39 +0200

    AVL - DONE!!!!

--- DataStructure/AVL Tree/AVL.h
+++ DataStructure/AVL Tree/AVL.h
@@ -4,319 +4,164 @@
 #include <algorithm>
 #include <stdexcept>
 #include <iostream>
+#include <memory>
 #include "Node.h"
 
-enum class Order{inOrder, postOrder, preOrder};
+enum class Order { inOrder, postOrder, preOrder };
 
 template<typename T>
 class AVL {
-    Node<T>* root;
+    std::unique_ptr<Node<T>> root;
     int count;
 
-    int calcHeightDiff(Node<T>* node){
-        int hLeft = (node->left != nullptr ? node->left->height : -1);
-        int hRight = (node->right != nullptr ? node->right->height : -1);
+    int calcHeightDiff(Node<T>* node) {
+        int hLeft = (node->left ? node->left->height : -1);
+        int hRight = (node->right ? node->right->height : -1);
         return hLeft - hRight;
     }
 
-    void rotateLeft(Node<T>* node, bool dir = true){
-        auto parent = node;
-        auto child = node->right;
-
-        node = node->right;
-        node->parent->right = node->left;
-        if(node->left) {
-            node->left->parent = node->parent;
-        }
-        node->left = node->parent;
-        auto temp = node->parent->parent;
-        if(node->parent->parent) {
-            if (dir)
-                node->parent->parent->right = node;
-            else
-                node->parent->parent->left = node;
-        }
-        node->parent->parent = node;
-        node->parent = temp;
-
-        if(parent == root){
-            root = child;
-        }
-
-        parent->height = 1 + std::max((parent->left != nullptr ? parent->left->height : -1), (parent->right != nullptr ? parent->right->height : -1));
-        child->height = 1 + std::max((child->left != nullptr ? child->left->height : -1), (child->right != nullptr ? child->right->height : -1));
+    void updateHeight(Node<T>* node) {
+        node->height = 1 + std::max((node->left ? node->left->height : -1), (node->right ? node->right->height : -1));
     }
 
-    void rotateRight(Node<T>* node, bool dir = true){
-        auto parent = node;
-        auto child = node->left;
-
-        node = node->left;
-        node->parent->left = node->right;
-        if(node->right) {
-            node->right->parent = node->parent;
-        }
-        node->right = node->parent;
-        auto temp = node->parent->parent;
-        if(node->parent->parent) {
-            if (dir)
-                node->parent->parent->left = node;
-            else
-                node->parent->parent->right = node;
-        }
-        node->parent->parent = node;
-        node->parent = temp;
-
-        if(parent == root){
-            root = child;
-        }
-
-        parent->height = 1 + std::max((parent->left != nullptr ? parent->left->height : -1), (parent->right != nullptr ? parent->right->height : -1));
-        child->height = 1 + std::max((child->left != nullptr ? child->left->height : -1), (child->right != nullptr ? child->right->height : -1));
+    void rotateLeft(Node<T>*& node) {
+        auto child = std::move(node->right);
+        node->right = std::move(child->left);
+        if (node->right) node->right->parent = node;
+        child->left = std::move(node);
+        child->left->parent = child.get();
+        node = std::move(child);
+        updateHeight(node->left.get());
+        updateHeight(node->get());
     }
 
-    void in_Order(Node<T>* node){
-        if(node == nullptr)
-            return;
-        in_Order(node->left);
-        std::cout<<node->val<<" ";
-        in_Order(node->right);
+    void rotateRight(Node<T>*& node) {
+        auto child = std::move(node->left);
+        node->left = std::move(child->right);
+        if (node->left) node->left->parent = node;
+        child->right = std::move(node);
+        child->right->parent = child.get();
+        node = std::move(child);
+        updateHeight(node->right.get());
+        updateHeight(node->get());
     }
 
-    void pre_Order(Node<T>* node){
-        if(node == nullptr)
-            return;
-        std::cout<<node->val<<" ";
-        pre_Order(node->left);
-        pre_Order(node->right);
+    void balance(Node<T>*& node) {
+        updateHeight(node);
+        int balanceFactor = calcHeightDiff(node);
+        if (balanceFactor == 2) {
+            if (calcHeightDiff(node->left.get()) < 0) {
+                rotateLeft(node->left);
+            }
+            rotateRight(node);
+        } else if (balanceFactor == -2) {
+            if (calcHeightDiff(node->right.get()) > 0) {
+                rotateRight(node->right);
+            }
+            rotateLeft(node);
+        }
     }
 
-    void post_Order(Node<T>* node){
-        if(node == nullptr)
-            return;
-        post_Order(node->left);
-        post_Order(node->right);
-        std::cout<<node->val<<" ";
+    void insertP(std::unique_ptr<Node<T>>& node, T val, Node<T>* parent) {
+        if (!node) {
+            node = std::make_unique<Node<T>>(val, parent);
+            count++;
+        } else if (val < node->val) {
+            insert(reinterpret_cast<std::unique_ptr<Node<int>> &>(node->left), val, node.get());
+        } else if (val > node->val) {
+            insert(node->right, val, node.get());
+        } else {
+            throw std::invalid_argument("duplicates are not allowed, value already exists");
+        }
+        balance(node);
+    }
+
+    void eraseP(std::unique_ptr<Node<T>>& node, T val) {
+        if (!node) {
+            throw std::invalid_argument("value not present");
+        } else if (val < node->val) {
+            erase(node->left, val);
+        } else if (val > node->val) {
+            erase(node->right, val);
+        } else {
+            if (!node->left) {
+                node = std::move(node->right);
+            } else if (!node->right) {
+                node = std::move(node->left);
+            } else {
+                auto minNode = findMin(node->right.get());
+                node->val = minNode->val;
+                erase(node->right, minNode->val);
+            }
+            count--;
+        }
+        if (node) balance(node);
+    }
+
+    Node<T>* findMin(Node<T>* node) const {
+        while (node->left) node = node->left.get();
+        return node;
+    }
+
+    void inOrder(Node<T>* node) const {
+        if (!node) return;
+        inOrder(node->left.get());
+        std::cout << node->val << " ";
+        inOrder(node->right.get());
+    }
+
+    void preOrder(Node<T>* node) const {
+        if (!node) return;
+        std::cout << node->val << " ";
+        preOrder(node->left.get());
+        preOrder(node->right.get());
+    }
+
+    void postOrder(Node<T>* node) const {
+        if (!node) return;
+        postOrder(node->left.get());
+        postOrder(node->right.get());
+        std::cout << node->val << " ";
     }
 
 public:
-    AVL(){
-        root = nullptr;
-        count = 0;
-    }
+    AVL() : root(nullptr), count(0) {}
 
-    int size(){
+    int size() const {
         return count;
     }
 
-    bool empty(){
+    bool empty() const {
         return count == 0;
     }
 
-    Node<T>* search(T val){
-        auto temp = root;
-        while(temp != nullptr){
-            if(temp->val == val)
-                return temp;
-            if(val < temp->val)
-                temp = temp->left;
-            else
-                temp = temp->right;
+    Node<T>* search(T val) const {
+        auto temp = root.get();
+        while (temp) {
+            if (temp->val == val) return temp;
+            temp = (val < temp->val) ? temp->left.get() : temp->right.get();
         }
         return nullptr;
     }
 
-    void insert(T val){
-        //Check for duplicate value
-        if (search(val) != nullptr){
-            throw std::invalid_argument("duplicates are not allowed, value already exists");
-        }
-
-        //Increment nodes counter
-        count++;
-        //If no nodes yet add the root
-        if(root == nullptr){
-            root = new Node<T>(val);
-            return;
-        }
-
-        //Put new node
-        auto temp = root;
-        while(true){
-            if(val > temp->val){
-                temp->height = std::max(temp->height, (temp->right != nullptr ? temp->right->height + 2 : 1));
-                if(temp->right == nullptr){
-                    temp->right = new Node<T>(val, temp);
-                    break;
-                }
-                temp = temp->right;
-            }
-            else{
-                temp->height = std::max(temp->height, (temp->left != nullptr ? temp->left->height + 2 : 1));
-                if(temp->left == nullptr){
-                    temp->left = new Node<T>(val, temp);
-                    break;
-                }
-                temp = temp->left;
-            }
-        }
-
-        //Re-balance the tree
-        while(temp != nullptr){
-            int heightDiff = calcHeightDiff(temp);
-            if(heightDiff == 2){
-                if(calcHeightDiff(temp->left) >= 0){
-                    rotateRight(temp);
-                }
-                else{
-                    rotateLeft(temp->left, false);
-                    rotateRight(temp);
-                }
-            }
-            else if(heightDiff == -2){
-                if(calcHeightDiff(temp->right) <= 0){
-                    rotateLeft(temp);
-                }
-                else{
-                    rotateRight(temp->right, false);
-                    rotateLeft(temp);
-                }
-            }
-            temp = temp->parent;
-        }
+    void insert(T val, int i, Node<int> *pNode) {
+        insertP(root, val, nullptr);
     }
 
-    void erase(T val, bool is_erase = true){
-        auto temp = search(val);
-        if(temp == nullptr){
-            throw std::invalid_argument("value not present");
-        }
-
-        count -= is_erase;
-        if(root->val == val){
-            if(root->left == nullptr && root->right == nullptr){
-                delete root;
-            }
-            else if(root->right == nullptr){
-                auto parent = root->left;
-                delete root;
-                parent->parent = nullptr;
-                root = parent;
-            }
-            else if(root->left == nullptr){
-                auto parent = root->right;
-                delete root;
-                parent->parent = nullptr;
-                root = parent;
-            }
-            else{
-                auto parent = root->right;
-                T tmpVal;
-                while(parent->left != nullptr){
-                    parent = parent->left;
-                }
-                tmpVal = parent->val;
-                erase(parent->val, false);
-                root->val = tmpVal;
-            }
-        }
-        else{
-            auto parent = temp->parent;
-            if(parent->left != nullptr && parent->left->val == val){
-                if(parent->left->left == nullptr && parent->left->right == nullptr){
-                    delete parent->left;
-                    parent->left = nullptr;
-                }
-                else if(parent->left->left == nullptr){
-                    parent->left->right->parent = parent;
-                    auto child = parent->left->right;
-                    delete parent->left;
-                    parent->left = child;
-                }
-                else if(parent->left->right == nullptr){
-                    parent->left->left->parent = parent;
-                    auto child = parent->left->left;
-                    delete parent->left;
-                    parent->left = child;
-                }
-                else{
-                    auto child = parent->left->right;
-                    T tmpVal;
-                    while(child->left != nullptr){
-                        child = child->left;
-                    }
-                    tmpVal = child->val;
-                    erase(tmpVal, false);
-                    parent->left->val = tmpVal;
-                }
-            }
-            else{
-                if(parent->right->left == nullptr && parent->right->right == nullptr){
-                    delete parent->right;
-                    parent->right = nullptr;
-                }
-                else if(parent->right->left == nullptr){
-                    parent->right->right->parent = parent;
-                    auto child = parent->right->right;
-                    delete parent->right;
-                    parent->right = child;
-                }
-                else if(parent->right->right == nullptr){
-                    parent->right->left->parent = parent;
-                    auto child = parent->right->left;
-                    delete parent->right;
-                    parent->right = child;
-                }
-                else{
-                    auto child = parent->right->right;
-                    T tmpVal;
-                    while(child->left != nullptr){
-                        child = child->left;
-                    }
-                    tmpVal = child->val;
-                    erase(tmpVal, false);
-                    parent->right->val = tmpVal;
-                }
-            }
-        }
-
-        if(is_erase) {
-            //Re-balance the tree
-            while (temp != nullptr) {
-                int heightDiff = calcHeightDiff(temp);
-                if (heightDiff == 2) {
-                    if (calcHeightDiff(temp->left) >= 0) {
-                        rotateRight(temp);
-                    } else {
-                        rotateLeft(temp->left, false);
-                        rotateRight(temp);
-                    }
-                } else if (heightDiff == -2) {
-                    if (calcHeightDiff(temp->right) <= 0) {
-                        rotateLeft(temp);
-                    } else {
-                        rotateRight(temp->right, false);
-                        rotateLeft(temp);
-                    }
-                }
-                temp = temp->parent;
-            }
-        }
+    void erase(T val) {
+        eraseP(root, val);
     }
 
-    void print(Order order = Order::inOrder){
-        if(order == Order::inOrder){
-            in_Order(root);
+    void print(Order order = Order::inOrder) const {
+        if (order == Order::inOrder) {
+            inOrder(root.get());
+        } else if (order == Order::postOrder) {
+            postOrder(root.get());
+        } else {
+            preOrder(root.get());
         }
-        else if(order == Order::postOrder){
-            post_Order(root);
-        }
-        else{
-            pre_Order(root);
-        }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
 };
 
-
-#endif //AVL_TREE_AVL_H
+#endif // AVL_TREE_AVL_H
